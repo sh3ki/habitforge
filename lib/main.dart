728b9ef93e3f9a5f32656cb519f03a6163cb47f7
sh3ki <shekaigarcia@gmail.com>
2026-03-08 17:50:57 +0800

    Create lib/main.dart

---
+++ lib/main.dart
@@ -0,0 +1,29 @@
+import 'package:flutter/material.dart';
+import 'package:google_fonts/google_fonts.dart';
+import 'package:flutter/services.dart';
+import 'theme/app_theme.dart';
+import 'screens/splash_screen.dart';
+
+void main() {
+  WidgetsFlutterBinding.ensureInitialized();
+  SystemChrome.setPreferredOrientations([
+    DeviceOrientation.portraitUp,
+    DeviceOrientation.portraitDown,
+  ]);
+  GoogleFonts.config.allowRuntimeFetching = false;
+  runApp(const HabitForgeApp());
+}
+
+class HabitForgeApp extends StatelessWidget {
+  const HabitForgeApp({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      title: 'HabitForge',
+      debugShowCheckedModeBanner: false,
+      theme: AppTheme.lightTheme,
+      home: const SplashScreen(),
+    );
+  }
+}
