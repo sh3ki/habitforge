b4d1368b779d48acea91ac02be35092d1c162449
sh3ki <shekaigarcia@gmail.com>
2026-03-11 19:46:24 +0800

    Update lib/widgets/app_logo.dart

--- lib/widgets/app_logo.dart
+++ lib/widgets/app_logo.dart
@@ -4,14 +4,8 @@ import '../theme/app_theme.dart';
 class AppLogo extends StatelessWidget {
   final double size;
   final bool showText;
-  final bool darkBackground;
 
-  const AppLogo({
-    super.key,
-    this.size = 48,
-    this.showText = true,
-    this.darkBackground = false,
-  });
+  const AppLogo({super.key, this.size = 48, this.showText = true});
 
   @override
   Widget build(BuildContext context) {
@@ -22,44 +16,17 @@ class AppLogo extends StatelessWidget {
           width: size,
           height: size,
           decoration: BoxDecoration(
-            gradient: const LinearGradient(
-              colors: [Color(0xFFFF6B35), Color(0xFFFF9800)],
-              begin: Alignment.topLeft,
-              end: Alignment.bottomRight,
-            ),
+            color: AppTheme.primary,
             borderRadius: BorderRadius.circular(size * 0.25),
-            boxShadow: [
-              BoxShadow(
-                color: AppTheme.primary.withOpacity(0.4),
-                blurRadius: 12,
-                offset: const Offset(0, 4),
-              ),
-            ],
           ),
           child: Center(
-            child: Stack(
-              alignment: Alignment.center,
-              children: [
-                Icon(Icons.bolt_rounded,
-                    color: Colors.white, size: size * 0.6),
-                Positioned(
-                  bottom: size * 0.06,
-                  right: size * 0.06,
-                  child: Container(
-                    width: size * 0.3,
-                    height: size * 0.3,
-                    decoration: BoxDecoration(
-                      color: const Color(0xFFFFC107),
-                      shape: BoxShape.circle,
-                      border: Border.all(color: Colors.white, width: 1.5),
-                    ),
-                    child: Center(
-                      child: Icon(Icons.local_fire_department_rounded,
-                          color: Colors.white, size: size * 0.18),
-                    ),
-                  ),
-                ),
-              ],
+            child: Text(
+              'H',
+              style: TextStyle(
+                fontSize: size * 0.52,
+                fontWeight: FontWeight.w900,
+                color: AppTheme.secondary,
+              ),
             ),
           ),
         ),
@@ -71,22 +38,18 @@ class AppLogo extends StatelessWidget {
                 TextSpan(
                   text: 'Habit',
                   style: TextStyle(
-                    fontFamily: 'Nunito',
-                    fontSize: size * 0.5,
+                    fontSize: size * 0.45,
                     fontWeight: FontWeight.w800,
-                    color: darkBackground
-                        ? Colors.white
-                        : AppTheme.textPrimary,
+                    color: AppTheme.textPrimary,
                     letterSpacing: -0.3,
                   ),
                 ),
                 TextSpan(
                   text: 'Forge',
                   style: TextStyle(
-                    fontFamily: 'Nunito',
-                    fontSize: size * 0.5,
+                    fontSize: size * 0.45,
                     fontWeight: FontWeight.w800,
-                    color: AppTheme.primary,
+                    color: AppTheme.secondary,
                     letterSpacing: -0.3,
                   ),
                 ),
