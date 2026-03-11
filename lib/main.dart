3a3dc8403267d2e72de74a4fadfbb09539f0854d
sh3ki <shekaigarcia@gmail.com>
2026-03-11 19:46:22 +0800

    Update lib/main.dart

--- lib/main.dart
+++ lib/main.dart
@@ -1,5 +1,4 @@
 import 'package:flutter/material.dart';
-import 'package:google_fonts/google_fonts.dart';
 import 'package:flutter/services.dart';
 import 'theme/app_theme.dart';
 import 'screens/splash_screen.dart';
@@ -10,7 +9,6 @@ void main() {
     DeviceOrientation.portraitUp,
     DeviceOrientation.portraitDown,
   ]);
-  GoogleFonts.config.allowRuntimeFetching = false;
   runApp(const HabitForgeApp());
 }
 
